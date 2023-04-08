e3b0e0a2eee8365fb4212d3d418b19f413f43f76
Yuliia Balabukha <balabukha08@ukr.net>
2023-04-08 14:29:10 +0300

    1-3

---
+++ src/js/03-feedback.js
@@ -0,0 +1,27 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const { email, message } = formEl.elements;
+
+
+formEl.addEventListener('input', throttle((e) => {
+    localStorage.setItem('feedback-form-state', JSON.stringify({
+        email: email.value,
+        message: message.value,
+    }))
+}, 500))
+
+
+const dataUserForm = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (dataUserForm) {
+    email.value = dataUserForm.email;
+    message.value = dataUserForm.message;
+}
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(localStorage.getItem('feedback-form-state'));
+    localStorage.clear();
+    formEl.reset();
+})
